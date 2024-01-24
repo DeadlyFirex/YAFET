6f52c26756b3f21a069a8c90d693a6bdbede67bf
Deadly <info.saddiki@gmail.com>
2024-01-24 23:56:52 +0100

    First attempt at decryption

--- main/app/main.cpp
+++ main/app/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "core.h"
 
 int main() {
+    std::string encryptionKey = "0123456789abcdef";
+
+    // Input and output file paths
+    std::string inputFilePath = "example.txt";
+    std::string encryptedOutputFilePath = "example_encrypted.txt";
+    std::string decryptedOutputFilePath = "example_decrypted.txt";
+
+    // Encrypt the file
+    encryptFile(inputFilePath, encryptedOutputFilePath, encryptionKey);
+    decryptFile(encryptedOutputFilePath, decryptedOutputFilePath, encryptionKey);
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
