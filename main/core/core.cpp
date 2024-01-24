ebf63a993b26594f339e4bfc0d804f912b4972e2
Deadly <info.saddiki@gmail.com>
2024-01-24 23:58:29 +0100

    Updated core with new functionality

--- main/core/core.cpp
+++ main/core/core.cpp
@@ -1,1 +1,64 @@
+#include "core.h"
 
+using namespace std;
+using namespace CryptoPP;
+
+using namespace core;
+
+bool encryptFile(const std::string& inputFilePath, const std::string& outputFilePath, const std::string& key) {
+    // Read the input file
+    std::ifstream inputFile(inputFilePath, std::ios::binary);
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening input file: " << inputFilePath << std::endl;
+        return false;
+    }
+
+    // Prepare the output file
+    std::ofstream outputFile(outputFilePath, std::ios::binary);
+    if (!outputFile.is_open()) {
+        std::cerr << "Error opening output file: " << outputFilePath << std::endl;
+        return false;
+    }
+
+    // Set up AES encryption
+    CryptoPP::AES::Encryption aesEncryption((CryptoPP::byte*)key.data(), CryptoPP::AES::DEFAULT_KEYLENGTH);
+    CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption(aesEncryption, (CryptoPP::byte*)key.data());
+
+    // Use Crypto++ filters for encryption
+    CryptoPP::FileSource fileSource(inputFile, true,
+                                    new CryptoPP::StreamTransformationFilter(cbcEncryption,
+                                                                             new CryptoPP::FileSink(outputFile)
+                                    )
+    );
+
+    return true;
+}
+
+bool decryptFile(const std::string& inputFilePath, const std::string& outputFilePath, const std::string& key) {
+    // Read the input file
+    std::ifstream inputFile(inputFilePath, std::ios::binary);
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening input file: " << inputFilePath << std::endl;
+        return false;
+    }
+
+    // Prepare the output file
+    std::ofstream outputFile(outputFilePath, std::ios::binary);
+    if (!outputFile.is_open()) {
+        std::cerr << "Error opening output file: " << outputFilePath << std::endl;
+        return false;
+    }
+
+    // Set up AES encryption
+    CryptoPP::AES::Encryption aesEncryption((CryptoPP::byte*)key.data(), CryptoPP::AES::DEFAULT_KEYLENGTH);
+    CryptoPP::CBC_Mode_ExternalCipher::Decryption cbcDecryption(aesEncryption, (CryptoPP::byte*)key.data());
+
+    // Use Crypto++ filters for encryption
+    CryptoPP::FileSource fileSource(inputFile, true,
+                                    new CryptoPP::StreamTransformationFilter(cbcDecryption,
+                                                                             new CryptoPP::FileSink(outputFile)
+                                    )
+    );
+
+    return true;
+}
