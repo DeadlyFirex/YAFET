cde35563bff964536172698ab24d54b8c8c717a8
Deadly <info.saddiki@gmail.com>
2024-01-24 23:57:48 +0100

    Added file skeleton header

--- main/classes/include/file.h
+++ main/classes/include/file.h
@@ -1,11 +1,43 @@
+#include <vector>
+#include <string>
+#include <ctime>
+
 namespace core {
 
-    class FileManager {
-
+    enum Algorithm {
+        AUTO = 0x00,
+        AES = 0x01
     };
 
     class File {
+    private:
+        std::string path;
+        std::size_t size;
+        std::time_t createdAt;
+        std::time_t modifiedAt;
+        std::string fileType;
+        std::string owner;
+        core::Algorithm algorithm;
 
+    public:
+        bool encrypted = false;
+        char version[6] = YAFET_VERSION;
+
+        explicit File(std::string & path);
+    };
+
+    class FileManager {
+    private:
+        std::vector<File> files;
+
+    public:
+        FileManager();
+        void addFile(File & file);
+        void removeFile(File & file);
+        void encryptAll();
+        void decryptAll();
+        void archiveTo(std::string & path, Algorithm algorithm);
+        void extractTo(std::string & path, Algorithm algorithm);
     };
 
 }
