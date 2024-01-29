a0446f29d04cfa57762320111e4a6a74dad367d8
Deadly <info.saddiki@gmail.com>
2024-01-29 23:13:03 +0100

    Added proper argument parsing

--- main/app/main.cpp
+++ main/app/main.cpp
@@ -1,18 +1,44 @@
 #include <iostream>
+#include <CLI/CLI.hpp>
+
 #include "core.h"
+#include "constants.h"
 
-int main() {
-    std::string encryptionKey = "0123456789abcdef";
+int main(int argc, char** argv) {
+    CLI::App app{yafetInfo};
 
-    // Input and output file paths
-    std::string inputFilePath = "example.txt";
-    std::string encryptedOutputFilePath = "example_encrypted.txt";
-    std::string decryptedOutputFilePath = "example_decrypted.txt";
+    std::string input;
+    std::string output;
+    std::string key;
 
-    // Encrypt the file
-    encryptFile(inputFilePath, encryptedOutputFilePath, encryptionKey);
-    decryptFile(encryptedOutputFilePath, decryptedOutputFilePath, encryptionKey);
+    app.add_flag_function("--version,-v", [&](int count) {
+        std::cout << core::yafetInfo;
+        exit(0);
+    }, "Show the version of the application");
 
-    std::cout << "Hello, World!" << std::endl;
+    CLI::App* encryptFile = app.add_subcommand("encrypt-file", "Encrypt a file");
+    encryptFile->add_option("input", input, "Specify the input file")->required();
+    encryptFile->add_option("output", output, "Specify the output file")->required();
+    encryptFile->add_option("key", key, "Specify the key to use")->required();
+
+    CLI::App* decryptFile = app.add_subcommand("decrypt-file", "Decrypt a file");
+    decryptFile->add_option("input", input, "Specify the input file")->required();
+    decryptFile->add_option("output", output, "Specify the output file")->required();
+    decryptFile->add_option("key", key, "Specify the key to use")->required();
+
+    CLI11_PARSE(app, argc, argv);
+
+    if (encryptFile->parsed()) {
+        std::ifstream inputFile = getIFileStream(input.c_str());
+        std::ofstream outputFile = getOFileStream(output.c_str());
+
+        encryptFileAes(inputFile, outputFile, key);
+    } else if (decryptFile->parsed()) {
+        std::ifstream inputFile = getIFileStream(input.c_str());
+        std::ofstream outputFile = getOFileStream(output.c_str());
+
+        decryptFileAes(inputFile, outputFile, key);
+    }
+
     return 0;
 }
