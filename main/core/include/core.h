2347ae7834b48b566429a67eaca33d313ed1d38f
Deadly <info.saddiki@gmail.com>
2024-01-29 23:16:03 +0100

    Added new functions, reworked old signatures

--- main/core/include/core.h
+++ main/core/include/core.h
@@ -3,10 +3,13 @@
 #include <cryptopp/cryptlib.h>
 #include <cryptopp/files.h>
 #include <cryptopp/aes.h>
+#include <cryptopp/blowfish.h>
 #include <cryptopp/modes.h>
 #include <cryptopp/filters.h>
 
 using namespace core;
 
-bool encryptFile(const std::string& inputFilePath, const std::string& outputFilePath, const std::string& key);
-bool decryptFile(const std::string& inputFilePath, const std::string& outputFilePath, const std::string& key);
+std::ifstream getIFileStream(const char * inputFilePath);
+std::ofstream getOFileStream(const char * inputFilePath);
+bool encryptFileAes(std::ifstream& inputFile, std::ofstream& outputFile, const std::string& key);
+bool decryptFileAes(std::ifstream& inputFile, std::ofstream& outputFile, const std::string& key);
