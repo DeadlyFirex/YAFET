a91851c96f51ed9a6e8a05d2dff9859ec731402f
Deadly <info.saddiki@gmail.com>
2024-01-29 23:17:59 +0100

    Added new constants header file to track constants

---
+++ main/core/include/constants.h
@@ -0,0 +1,9 @@
+namespace core {
+    const char yafetVersion[6] = YAFET_VERSION;
+    static const char * yafetInfo = " .======:              YAFET v" YAFET_VERSION "                       \n"
+                                    " :===============:     A lightweight and cross-platform C++23 utility \n"
+                                    " :===::::::::::::::    designed to effectively encrypt data.          \n"
+                                    " :==-:::::::::::::.                                                   \n"
+                                    " :=-::::::::::::::     This is free software, and you are welcome to  \n"
+                                    " .=-:::::::::::::.     redistribute it under the terms of the AGPLv3  \n";
+}
